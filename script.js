8626cab35cf4ade0fa2f87798d4f7198d9a3da44
Christopher Simmons <solidsnakespeare@gmail.com>
2024-02-29 15:54:14 -0500

    simple dark mode scheme working with all the buttons smile

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const body = document.getElementById("body");
 
 document.getElementById("osdefault").addEventListener("click", 
     function (event) {
-        body.classList.remove("dark", "light")
+        body.classList.add("osdefault")
+        body.classList.remove("dark")
     }, 
     false);
 
 document.getElementById("dark").addEventListener("click", 
     function (event) {
         body.classList.add("dark")
+        body.classList.remove("osdefault")
     }, 
     false);
 
 document.getElementById("light").addEventListener("click", 
     function (event) {
-        body.classList.remove("dark")
+        body.classList.remove("osdefault", "dark")
     }, 
     false);
